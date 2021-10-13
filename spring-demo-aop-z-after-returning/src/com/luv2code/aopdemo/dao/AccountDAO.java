e089f7af15a161738899cad3b3f1cafb3eb6f0ff
Vladut Margineanu <vladut.margineanu@gmail.com>
2021-10-13 22:41:43 +0300

    AOP - AfterReturning Advice Type

---
+++ spring-demo-aop-z-after-returning/src/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,73 @@
+package com.luv2code.aopdemo.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.luv2code.aopdemo.Account;
+
+@Component
+public class AccountDAO {
+
+	private String name;
+	private String serviceCode;
+	
+	// add a new method: findAccount()
+	
+	public List<Account> findAccounts() {
+		
+		List<Account> myAccounts = new ArrayList<>();
+		
+		// create sample accounts
+		Account temp1 = new Account("John", "Silver");
+		Account temp2 = new Account("Madhu", "Platinum");
+		Account temp3 = new Account("Luca", "Gold");
+		
+		// add them to our accounts list
+		myAccounts.add(temp1);
+		myAccounts.add(temp2);
+		myAccounts.add(temp3);
+		
+		return myAccounts;
+	}
+
+	public void addAccount() {
+		System.out.println(getClass() + ": Doing my DB work: Adding an account");
+	}
+
+	public void addAccount(Account theAccount) {
+		System.out.println(getClass() + ": Doing my DB work: Adding an account");
+	}
+
+	public void addAccount(Account theAccount, boolean vipFlag) {
+		System.out.println(getClass() + ": Doing my DB work: Adding an account");
+	}
+
+	public boolean doWork() {
+
+		System.out.println(getClass() + ": doWork()");
+		return false;
+	}
+
+	public String getName() {
+		System.out.println(getClass() + ": getName()");
+		return name;
+	}
+
+	public void setName(String name) {
+		System.out.println(getClass() + ": setName()");
+		this.name = name;
+	}
+
+	public String getServiceCode() {
+		System.out.println(getClass() + ": getServiceCode()");
+		return serviceCode;
+	}
+
+	public void setServiceCode(String serviceCode) {
+		System.out.println(getClass() + ": setServiceCode()");
+		this.serviceCode = serviceCode;
+	}
+
+}
---
+++ spring-demo-aop-z-after-returning/src/com/luv2code/aopdemo/dao/MembershipDAO.java
@@ -0,0 +1,26 @@
+package com.luv2code.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MembershipDAO {
+
+	public void addAccount() {
+		System.out.println(getClass() + ": Doing stuff: adding a membership account");
+	}
+	
+	public void addSillyMember() {
+		System.out.println(getClass() + ": Doing stuff: adding a membership account");
+	}
+	
+	public boolean addSillyMember1() {
+		System.out.println(getClass() + ": Doing stuff: adding a membership account");
+		
+		return true;
+	}
+	
+	public void goToSleep() {
+		
+		System.out.println(getClass() + ": I am going to sleep now");
+	}
+}
