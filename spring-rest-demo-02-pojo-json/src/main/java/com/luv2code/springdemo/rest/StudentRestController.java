a601178e7f91d76e35913dc165d084435d3fb676
Vladut Margineanu <vladut.margineanu@gmail.com>
2021-11-02 16:47:36 +0200

    Spring REST - Retrieve POJOs as JSON

---
+++ spring-rest-demo-02-pojo-json/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -0,0 +1,29 @@
+package com.luv2code.springdemo.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.springdemo.entity.Student;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+	// define endpoint for "/student"
+	// return a list of students
+
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+
+		List<Student> theStudents = new ArrayList<>();
+		theStudents.add(new Student("Dragos", "Sandulescu"));
+		theStudents.add(new Student("Vladut", "Margineanu"));
+		theStudents.add(new Student("Roxana", "Scurtu"));
+
+		return theStudents;
+	}
+}
