dd06b34941e15b11bc9992b55158a2bf766f2261
Vladut Margineanu <vladut.margineanu@gmail.com>
2021-11-16 17:01:00 +0200

    Spring Boot - Spring Data JPA

---
+++ 09-spring-boot-crud-spring-data-jpa/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	
+	// no need to write any code for CRUD operations
+
+}
